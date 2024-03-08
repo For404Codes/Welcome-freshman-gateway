8a4e135c4b8721cdac3e4f168a3124ff7681e176
For404Codes <zrj2214537@163.com>
2024-03-08 21:18:15 +0800

    添加跨域配置

--- src/main/java/com/righteous/innovate/wsgateway/WsGatewayApplication.java
+++ src/main/java/com/righteous/innovate/wsgateway/WsGatewayApplication.java
@@ -2,8 +2,10 @@ package com.righteous.innovate.wsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WsGatewayApplication {
 
     public static void main(String[] args) {
